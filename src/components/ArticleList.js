cc15cc42a93f4fe537fa7b11b209c8a7d7ff98a2
Alex Aguirre <aguirre_alex@rocketmail.com>
2021-09-24 23:13:54 -0500

    completed

---
+++ src/components/ArticleList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Article from './Article'
+
+export default function ArticleList({posts}) {
+    const postsArray = posts.map((post) => {
+        return <Article 
+                    key={post.id} 
+                    title={post.title}
+                    date={post.date}
+                    preview={post.preview}
+                />
+    })
+
+    return (
+        <main>
+            {postsArray}
+        </main>
+    )
+}
